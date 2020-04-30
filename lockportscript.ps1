acafb90e8d66bf7feb76d9f1fd95a188f0ffaa65
gertkaterbarg <63746051+gertkaterbarg@users.noreply.github.com>
2020-04-30 16:38:19 +0200

    Lockport script
    first approach to a simple script for locking a TCP port to be able to test how to deal with (unexpected) blockage of the port.

---
+++ lockportscript.ps1
@@ -0,0 +1,15 @@
+﻿
+$Response;
+[uint16]$port = Read-Host "which port do you want to block"
+
+do {
+$Listener = [System.Net.Sockets.TcpListener]$port;
+$Listener.Start();
+
+Write-Output $Listener
+#wait, try connect from another PC etc.
+#Start-Sleep -s 10
+$Response = Read-Host "Do you want to stop the lock? (Y)"
+}until ($Response -eq "y");
+
+$Listener.Stop();
